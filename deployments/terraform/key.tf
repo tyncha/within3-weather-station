f1016ccc824b66882f1cf60572bf3a95ddd08325
tyncha <tyncha87@gmail.com>
2022-10-22 18:08:04 -0500

    Disabled all the nodes

--- deployments/terraform/key.tf
+++ deployments/terraform/key.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "weatherkey" {
-  key_name   = "example-key1"
+  key_name   = "example-key2"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9OFQMsz1MCBDh0mFXhkF857/aX+TozfR0RCLXO8bIYBe2QDNgvq4+VJmoXe1OzvopL0xv+MgDyTLwGPuX1w/H9iCEZVHPBtZrtCMWFdIK7OwlxO3b8RvhzR3s+hM5XS5F3GFTgyrxiVGX6MgLSUkNvkINaZpyAiueYbpO/hMP1X/sPgEM5PtF6V2qz9b9F/H9Tq9757AIIZQpTTsqo7Ye/pbuIam0o41T/7Zw1NI8z8SXUjnm5jdoaKZrLvstf9piSE6hQ5PlyUm6PeW8cTynAn/zM3be3L8ETBh838F/bFoAroB+XoXZS3sdH9Udd/7o+QskfJLsRV8bBhVe1S/r tyncha@bastion-fuchicorp-com"
 }
