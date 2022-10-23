b5b290edcba6d535a4d3e8ac813298a8c323a4a4
tyncha <tyncha87@gmail.com>
2022-10-23 12:07:41 -0500

    Merge branch 'master' of github.com:tyncha/within3-weather-station

---
+++ deployments/.history/terraform/iam_20221023091616.tf
@@ -0,0 +1,57 @@
+resource "aws_iam_role_policy" "ec2_policy" {
+  name = "ec2_policy"
+  role = aws_iam_role.ec2_role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Action": "ec2:*",
+        "Resource": "*"
+         Effect"   = "Allow"
+        
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "new_ec2_profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_organizations_policy" "example" {
+  name = "example"
+
+  content = <<CONTENT
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Action": "*",
+    "Resource": "*"
+  }
+}
+CONTENT
+}
--- deployments/.history/terraform/instance_20221023012151.tf
+++ deployments/.history/terraform/instance_20221023012151.tf
@@ -1,16 +1,13 @@
 resource "aws_instance" "web" {
   ami                         = "ami-09d3b3274b6c5d4aa"
   instance_type               = "t2.micro"
-  iam_instance_profile        = "${aws_iam_instance_profile.ec2_profile.name}"
+  iam_instance_profile        = "${aws_iam_instance_profile.ec2_profile}"
   associate_public_ip_address = "true"
-  key_name                    = "${aws_key_pair.weather_key.key_name}"
+  key_name                    = "${aws_key_pair.weatherkey.key_name}"
+  user_data                   = "${file("userdata_file.sh")}"
   vpc_security_group_ids      = ["${aws_security_group.example-new1.id}"]
   availability_zone           = "us-east-1a"
   tags = {
     Name = "weater7"
   }
 }
-
-
-
-
