e410074c6b6a722b2584c6b168b4225ab459750b
tyncha <tyncha87@gmail.com>
2022-10-23 13:23:52 -0500

    Disabled all the nodes

--- deployments/terraform/instance.tf
+++ deployments/terraform/instance.tf
@@ -4,10 +4,11 @@ resource "aws_instance" "web" {
   iam_instance_profile        = "${aws_iam_instance_profile.ec2_profile.name}"
   associate_public_ip_address = "true"
   key_name                    = "${aws_key_pair.weather_key.key_name}"
+  user_data                   = "${file("userdata_file.sh")}"
   vpc_security_group_ids      = ["${aws_security_group.example-new2.id}"]
   availability_zone           = "us-east-1a"
   tags = {
-    Name = "weater7"
+    Name = "weather7"
   }
 }
 
---
+++ deployments/terraform/userdata_file.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+docker run -ti -p 80:80 tyncha/weather-station
