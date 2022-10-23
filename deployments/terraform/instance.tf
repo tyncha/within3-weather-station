8d61c02a3c32b16091a49fb6d0c14f30118f05bd
tyncha <tyncha87@gmail.com>
2022-10-23 12:40:17 -0500

    Disabled all the nodes

--- deployments/terraform/instance.tf
+++ deployments/terraform/instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
   iam_instance_profile        = "${aws_iam_instance_profile.ec2_profile.name}"
   associate_public_ip_address = "true"
   key_name                    = "${aws_key_pair.weather_key.key_name}"
-  vpc_security_group_ids      = ["${aws_security_group.example-new1.id}"]
+  vpc_security_group_ids      = ["${aws_security_group.example-new2.id}"]
   availability_zone           = "us-east-1a"
   tags = {
     Name = "weater7"
