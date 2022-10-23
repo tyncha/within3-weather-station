94f114c8808f40707369c15187dfd54b76da455b
tyncha <tyncha87@gmail.com>
2022-10-23 17:24:30 -0500

    Disabled all the nodes

--- deployments/docker/index.js
+++ deployments/docker/index.js
@@ -13,6 +13,6 @@ app.get( "/", ( req, res ) => {
 } );
 
 
-app.listen(port,'0.0.0.0',()=>{
-      console.log(`Server is listening on ${port} and binded 0.0.0.0 port`);
+app.listen(port, '0.0.0.0', ()=>{
+    console.log(`Server is listening on ${port} and binded 0.0.0.0 port`);
 })
